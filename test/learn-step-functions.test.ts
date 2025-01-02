35415a9590c543bfae70e5ac340d8d7746ba2ddd
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2025-01-03 03:52:35 +0900

    Initial commit

---
+++ test/learn-step-functions.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LearnStepFunctions from '../lib/learn-step-functions-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/learn-step-functions-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LearnStepFunctions.LearnStepFunctionsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
