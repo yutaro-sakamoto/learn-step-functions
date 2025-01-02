f27cf0b848a13d0828f42cb0925279ee10167b9b
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2025-01-03 04:54:59 +0900

    doc: add typedoc comments

--- lib/FirstExample/first-example.ts
+++ lib/FirstExample/first-example.ts
@@ -1,6 +1,9 @@
 import { Construct } from "constructs";
 import * as sfn from "aws-cdk-lib/aws-stepfunctions";
 
+/**
+ * The first example of Step Functions
+ */
 export class FirstExample extends Construct {
   constructor(scope: Construct, id: string) {
     super(scope, id);
--- lib/learn-step-functions-stack.ts
+++ lib/learn-step-functions-stack.ts
@@ -2,6 +2,10 @@ import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { FirstExample } from "./FirstExample/first-example";
 
+/**
+ * Step Functions examples
+ */
+
 export class LearnStepFunctionsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
