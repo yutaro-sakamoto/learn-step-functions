3adaf45523827f7a20536d51c1b8228def032d25
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2025-01-07 21:30:08 +0900

    chore: eslint ignores lib/FirstExample/first-example.function.ts

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,7 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 export default [
-  { ignores: ["**/*.js"] },
+  { ignores: ["**/*.js", "lib/FirstExample/first-example.function.ts"] },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ];
